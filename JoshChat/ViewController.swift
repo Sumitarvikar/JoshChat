b37819bfecc66a3d478604946b12fab2c9dd5fe7
Sumitarviakr <sumitarvikar77@gmail.com>
2022-10-01 15:32:58 +0530

    Intial Setup

---
+++ JoshChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JoshChat
+//
+//  Created by Josh on 01/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
